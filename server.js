219cd3ad445fbd4297a52ceed287b9189fc3495a
krarchi <kumar.archi.anand@symbtechnologies.com>
2020-06-05 11:25:50 +0530

    sixth check commit

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/MY_APP_NAME_HERE'));
+app.use(express.static(__dirname + '/dist/task-sheet-symb'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/MY_APP_NAME_HERE/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/task-sheet-symb/index.html'));
 });
 
 // default Heroku PORT
 app.listen(process.env.PORT || 3000);
+
