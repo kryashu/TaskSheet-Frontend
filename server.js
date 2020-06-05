6e01d4ea5203e1b85a36eff7b3ef95e689b8b6e7
krarchi <kumar.archi.anand@symbtechnologies.com>
2020-06-05 13:33:06 +0530

    check

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/task-sheet-symb'));
+app.use(express.static(__dirname + '/dist'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join('index.html' + '/dist/task-sheet-symb/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // default Heroku PORT
 app.listen(process.env.PORT || 3000);
-
